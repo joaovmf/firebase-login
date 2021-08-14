7374133006759f785975e7675081f88a13de4d96
João Victor de Medeiros <medeirosjoaoadv@gmail.com>
2021-08-14 17:08:00 -0300

    correção reader login

--- src/components/Novo-Login/LoginPage.js
+++ src/components/Novo-Login/LoginPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import HeaderLogin from "./Header/HeaderLogin"
+import HeaderLogin from "./Header/HeaderLogin";
 
 const LoginPage = (props) => {
   const {
@@ -19,86 +19,85 @@ const LoginPage = (props) => {
 
   return (
     <>
-    <HeaderLogin />
-    <section className="login">
-      <div className="loginContainer">
-        {hasAccount ? (
-         <>
-         <label>Usuário</label>
-         <input
-           type="text"
-           autoFocus
-           require
-           value={email}
-           onChange={(e) => setEmail(e.target.value)}
-         />
-         <p className="errorMsg">{emailError}</p>
-         <label>Senha</label>
-         <input
-           type="password"
-           require
-           value={password}
-           onChange={(e) => setPassword(e.target.value)}
-         />
-         <label>Confirmar Senha</label>
-         <input
-           type="password"
-           require
-           value={confirmPassword}
-           onChange={(e) => setConfirmPassword(e.target.value)}
-         />
-         <p className="errorMsg">{passwordError}</p>
-       </>
-        ) : (
-          <>
-          <label>Usuário</label>
-          <input
-            type="text"
-            autoFocus
-            require
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <p className="errorMsg">{emailError}</p>
-          <label>Senha</label>
-          <input
-            type="password"
-            require
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <p className="errorMsg">{passwordError}</p>
-        </>
-        
-        )}
-        <div className="btnContainer">
+      <HeaderLogin />
+      <section className="login">
+        <div className="loginContainer">
           {hasAccount ? (
             <>
-              <button onClick={handleLogin} className="btn">
-                Entre
-              </button>
-              <p>
-                {" "}
-                Não tem uma conta ?{" "}
-                <span onClick={() => setHasAccount(!hasAccount)}>
-                  Se inscreva
-                </span>
-              </p>
+              <label>Usuário</label>
+              <input
+                type="text"
+                autoFocus
+                require
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <p className="errorMsg">{emailError}</p>
+              <label>Senha</label>
+              <input
+                type="password"
+                require
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label>Confirmar Senha</label>
+              <input
+                type="password"
+                require
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <p className="errorMsg">{passwordError}</p>
             </>
           ) : (
             <>
-              <button onClick={handleSignup} className="btn">
-                Se inscreva
-              </button>
-              <p>
-                Tem uma conta ?{" "}
-                <span onClick={() => setHasAccount(!hasAccount)}>Entre</span>
-              </p>
+              <label>Usuário</label>
+              <input
+                type="text"
+                autoFocus
+                require
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <p className="errorMsg">{emailError}</p>
+              <label>Senha</label>
+              <input
+                type="password"
+                require
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <p className="errorMsg">{passwordError}</p>
             </>
           )}
+          <div className="btnContainer">
+            {hasAccount ? (
+              <>
+                <button onClick={handleSignup} className="btn">
+                  Se inscreva
+                </button>
+                <p>
+                  Tem uma conta ?{" "}
+                  <span onClick={() => setHasAccount(!hasAccount)}>Entre</span>
+                </p>
+              </>
+            ) : (
+              <>
+                <button onClick={handleLogin} className="btn">
+                  Entre
+                </button>
+                <p>
+                  {" "}
+                  Não tem uma conta ?{" "}
+                  <span onClick={() => setHasAccount(!hasAccount)}>
+                    Se inscreva
+                  </span>
+                </p>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 };
